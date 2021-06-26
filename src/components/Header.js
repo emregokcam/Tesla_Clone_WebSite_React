516d83c61299c984782ad0874b579145db73a8e2
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2021-06-26 16:31:28 +0300

    added RightMenu

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,19 +8,15 @@ function Header() {
         <img src="/images/logo.svg" alt="tesla_logo" />
       </a>
       <Menu>
-        <p>
-          <a href="#">Model S</a>
-        </p>
-        <p>
-          <a href="#">Model 3</a>
-        </p>
-        <p>
-          <a href="#">Model X</a>
-        </p>
-        <p>
-          <a href="#">Model Y</a>
-        </p>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
       </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
     </Container>
   );
 }
@@ -44,10 +40,18 @@ const Menu = styled.div`
   justify-content: center;
   flex: 1;
 
-  p {
+  a {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
+  }
+`;
+
+const RightMenu = styled.div`
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
   }
 `;
