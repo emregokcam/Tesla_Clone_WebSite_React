fe43184a81f0041ede9e6b3839cb046ea73ce535
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2021-06-26 14:06:23 +0300

    Updated CSS

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,10 +8,13 @@ function Section() {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src="images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -54,13 +57,15 @@ const LeftButton = styled.div`
   opacity: 0.85;
   text-transform: uppercase;
   font-size: 12px;
+  margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)`
-  /* width: 100vw;
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-image: url("/images/model-s.jpg"); */
+const RightButton = styled(LeftButton)``;
+
+const DownArrow = styled.img`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
 `;
+
+const Buttons = styled.div``;
